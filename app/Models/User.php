5aa7c004374e5dbda2ec60af70c6d9d935f9bfde
Nikos Paschalis <nikospasxalis94@gmail.com>
2026-01-04 19:44:40 +0200

    Chirp model added, chirp function added on User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Database\Eloquent\Relations\hasMany;
+
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
     }
 }
